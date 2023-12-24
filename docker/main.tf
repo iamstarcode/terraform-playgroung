c2ebd39012ead2c6d761dfeb65cc6490e14af5b0
Bakare Abiola <iamstarcode@gmail.com>
2023-12-24 01:23:59 +0100

    chore: update

---
+++ docker/main.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "3.0.2"
+    }
+  }
+}
+
+provider "docker" {
+  host = "unix:///home/starcode/.docker/desktop/docker.sock"
+}
+
+resource "docker_image" "nginx" {
+  name         = "nginx:1.25.3-alpine"
+  keep_locally = false
+}
+
+resource "docker_container" "nginx" {
+  image = docker_image.nginx.image_id
+  name  = "tutorial"
+
+  ports {
+    internal = 80
+    external = 8000
+  }
+}
